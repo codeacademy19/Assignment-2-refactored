0d344b9b755c65c0a136035cd8fe4c0909931594
Ajay B <ajay_b_balaji@external.mckinsey.com>
2019-02-08 13:21:06 +0530

    Helper and test to get Book rating by its ID

--- src/helpers/Books/index.js
+++ src/helpers/Books/index.js
@@ -5,4 +5,8 @@ module.exports = {
     const allBooksUrl = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
     return Axios(allBooksUrl).then(result => result.data);
   },
+  getRatingFromBookId: (id) => {
+    const externalUrl = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${id}`;
+    return Axios(externalUrl).then(apiResult => apiResult.data);
+  },
 };
--- tests/helpers/Books/index.test.js
+++ tests/helpers/Books/index.test.js
@@ -2,12 +2,18 @@
  * @jest-environment node
  */
 
-const { getBooksWithoutRating } = require('../../../src/helpers/Books');
+const { getBooksWithoutRating, getRatingFromBookId } = require('../../../src/helpers/Books');
 
 
 describe('getBooksWithoutRating', () => {
   it('should fetch an array of all the books from the given URL', async () => {
     const allBooks = await getBooksWithoutRating();
     expect(Array.isArray(allBooks.books)).toEqual(true);
+  });
+});
+describe('getRatingFromBookId', () => {
+  it('should fetch rating of a book with a given id', async () => {
+    const rating = await getRatingFromBookId(10);
+    expect(typeof (rating.rating)).toEqual('number');
   });
 });
